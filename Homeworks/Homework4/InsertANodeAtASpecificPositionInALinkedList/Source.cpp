f09c305952893dac0f2ee4b4605665b75f924f91
Stefania Tsvetkova <nia_flo@abv.bg>
2021-11-03 10:04:10 +0200

    Added insert a node at a specific position in a linked list problem

---
+++ Homeworks/Homework4/InsertANodeAtASpecificPositionInALinkedList/Source.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+public:
+    int data;
+    SinglyLinkedListNode* next;
+
+    SinglyLinkedListNode(int node_data) {
+        this->data = node_data;
+        this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+public:
+    SinglyLinkedListNode* head;
+    SinglyLinkedListNode* tail;
+
+    SinglyLinkedList() {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data) {
+        SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head) {
+            this->head = node;
+        }
+        else {
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+    }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
+    SinglyLinkedListNode* newNode = new SinglyLinkedListNode(data);
+
+    if (position == 0 || llist == nullptr)
+    {
+        newNode->next = llist;
+        llist = newNode;
+
+        return llist;
+    }
+
+    SinglyLinkedListNode* prevNode = llist;
+    for (int i = 1; i < position && prevNode->next != nullptr; i++)
+    {
+        prevNode = prevNode->next;
+    }
+
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+
+    return llist;
+}
+
+int main()
+{
+    ofstream fout;
+    fout.open("result.txt");
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
