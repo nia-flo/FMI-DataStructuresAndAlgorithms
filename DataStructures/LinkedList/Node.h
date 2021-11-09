fbec95fe7691f84068da196d3df90ef6366801e9
Stefania Tvetkova <nia_flo@abv.bg>
2021-11-09 16:54:40 +0200

    Update Node.h

--- DataStructures/LinkedList/Node.h
+++ DataStructures/LinkedList/Node.h
@@ -15,12 +15,3 @@ inline Node<T>::Node(const T& data, Node<T>* next)
 	this->data = data;
 	this->next = next;
 }
-
-//template<typename T>
-//inline Node<T>& Node<T>::operator=(const Node<T>& other)
-//{
-//	this->data = other.data;
-//	this->next = other.next;
-//
-//	return *this;
-//}
